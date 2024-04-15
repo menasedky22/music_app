8f3b3f98e898132fa3e8e2d76b5537d3d7bfb1bc
mena <menasedky22@gmail.com>
2024-04-15 16:55:16 +0200

    craete image to splash

---
+++ lib/core/resources/color_managers.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorManagers {
+  static const Color kPrimaryColor = Color(0xff411f5c);
+  static const Color kSecondPrimaryColor = Color(0xff2c17ca);
+}
--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/core/resources/color_managers.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+          //  height: double.infinity,
+          //   width: double.infinity,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: const Alignment(0.20, -0.89),
+                  end: const Alignment(-0.20, 0.89),
+                  colors: [
+                ColorManagers.kPrimaryColor,
+                ColorManagers.kSecondPrimaryColor.withOpacity(.54),
+              ])),
+          child: const Center(
+            child: Image(
+                width: 150,
+                height: 150,
+                image: AssetImage("assets/images/logo.png")),
+          )),
+    );
   }
 }
